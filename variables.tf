b48e006486aac6b3dd7cff34c62ea0f3e54b23e1
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-18 23:20:31 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "name" {
+    type = string
+    description = "The Name which should be used for this Resource Group"
+}
+
+variable "location" {
+    type = string
+    description = "The Azure Region where the Resource Group should exist."
+}
+
+
+
+
