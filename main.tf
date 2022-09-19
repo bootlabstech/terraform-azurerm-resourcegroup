07b6fc579d4e97b93d822577f2e2db018957bcf0
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-19 10:24:26 +0530

    adding depends on

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ resource "azurerm_management_lock" "resource-group-lock" {
   name       = "${var.name}-lock"
   scope      = azurerm_resource_group.resource_group.id
   lock_level = var.lock_level
+  depends_on = [
+    azurerm_resource_group.resource_group
+  ]
 }
 
 
