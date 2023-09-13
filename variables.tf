025523783b3be002897247d502c8dd496a9e3671
lalita2407 <lalitalallu02@gmail.com>
2023-09-13 18:45:58 +0530

    added keyvault resource block

--- variables.tf
+++ variables.tf
@@ -13,3 +13,49 @@ variable "location" {
 #   description = "Specifies the Level to be used for this Lock. Possible values are CanNotDelete and ReadOnly."
 #   default     = "ReadOnly"
 # }
+# Keyvault
+variable "name" {
+  type        = string
+  description = "Specifies the name of the Key Vault. Changing this forces a new resource to be created. The name must be globally unique. If the vault is in a recoverable state then the vault will need to be purged before reusing the name."
+}
+variable "sku_name" {
+  type        = string
+  description = "The Name of the SKU used for this Key Vault. Possible values are standard and premium."
+  default = "standard"
+}
+variable "enabled_for_deployment" {
+  type        = bool
+  description = "Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault."
+  default = true
+}
+variable "enabled_for_disk_encryption" {
+  type        = bool
+  description = " Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys."
+  default = true
+}
+variable "enabled_for_template_deployment" {
+  type        = bool
+  description = "Boolean flag to specify whether Azure Resource Manager is permitted to retrieve secrets from the key vault."
+  default = true
+}
+variable "enable_rbac_authorization" {
+  type        = bool
+  description = "Boolean flag to specify whether Azure Key Vault uses Role Based Access Control (RBAC) for authorization of data actions."
+  default = true
+}
+variable "purge_protection_enabled" {
+  type        = bool
+  description = "Is Purge Protection enabled for this Key Vault?"
+  default     = true
+}
+
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "Whether public network access is allowed for this Key Vault. Defaults to true."
+  default = false
+}
+variable "soft_delete_retention_days" {
+  type        = number
+  description = "The number of days that items should be retained for once soft-deleted. This value can be between 7 and 90 (the default) days."
+  default     = 7
+}
