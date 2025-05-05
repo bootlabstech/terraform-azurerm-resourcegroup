e8f36e8022d905faf15d7ed524b2c1934e1ca6a7
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-05 21:31:54 +0530

    updated the provider version

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.4.0" # Adjust this version as needed
+      version = "3.64.0"
     }
   }
 }
