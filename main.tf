16a7c1f803b82be14c393f297b8ddb6276dbcc0f
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-08 14:27:05 +0530

    using the new provider

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
+  name     = var.name
   location = var.location
   lifecycle {
     ignore_changes = [
@@ -7,49 +7,3 @@ resource "azurerm_resource_group" "resource_group" {
     ]
   }
 }
-data "azurerm_client_config" "current" {}
-
-# Creates a Key vault
-resource "azurerm_key_vault" "key_vault" {
-  name                            = var.keyvault_name
-  location                        = var.location
-  resource_group_name             = var.resource_group_name
-  sku_name                        = var.sku_name
-  tenant_id                       = data.azurerm_client_config.current.tenant_id
-  enabled_for_deployment          = var.enabled_for_deployment
-  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
-  enabled_for_template_deployment = var.enabled_for_template_deployment
-  enable_rbac_authorization       = var.enable_rbac_authorization
-  purge_protection_enabled        = var.purge_protection_enabled
-  public_network_access_enabled   = var.public_network_access_enabled
-  soft_delete_retention_days      = var.soft_delete_retention_days
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-    key_permissions = [
-      "SetRotationPolicy",
-    ]
-    secret_permissions = [
-      "Get", "Set", "List", "Recover", "Delete", "Restore"
-    ]
-    storage_permissions = [
-      "Get",
-    ]
-  }
-  network_acls {
-    bypass         = "AzureServices"
-    default_action = "Allow"
-
-  }
-  lifecycle {
-    ignore_changes = [
-      tags, access_policy
-    ]
-  }
-  depends_on = [azurerm_resource_group.resource_group]
-}
-
-
-
-
--- variables.tf
+++ variables.tf
@@ -1,43 +1,9 @@
-variable "resource_group_name" {
-  type = string
-}
-variable "location" {
-  type = string
-}
-# Keyvault
-variable "keyvault_name" {
-  type = string
-}
-variable "sku_name" {
-  type    = string
-  default = "standard"
-}
-variable "enabled_for_deployment" {
-  type    = bool
-  default = true
-}
-variable "enabled_for_disk_encryption" {
-  type    = bool
-  default = true
-}
-variable "enabled_for_template_deployment" {
-  type    = bool
-  default = true
-}
-variable "enable_rbac_authorization" {
-  type    = bool
-  default = false
-}
-variable "purge_protection_enabled" {
-  type    = bool
-  default = true
+variable "name" {
+  type        = string
+  description = "The Name which should be used for this Resource Group"
 }
 
-variable "public_network_access_enabled" {
-  type    = bool
-  default = true
-}
-variable "soft_delete_retention_days" {
-  type    = number
-  default = 7
+variable "location" {
+  type        = string
+  description = "The Azure Region where the Resource Group should exist."
 }
