61711cfc0a261764e6e37a9864dce706f6130eec
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-16 11:45:28 +0530

    updated the lifecycle block

--- main.tf
+++ main.tf
@@ -12,4 +12,9 @@ resource "azurerm_log_analytics_workspace" "central_workspace" {
   location            = "${azurerm_resource_group.resource_group.location}"
   resource_group_name = "${azurerm_resource_group.resource_group.name}"
   sku                 = "PerGB2018"
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
 }
