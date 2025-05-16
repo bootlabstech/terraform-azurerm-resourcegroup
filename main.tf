bb39641c58459dd3efa82ec56add474c1ac56d4c
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-16 10:32:26 +0530

    changed the rg and analytics

--- main.tf
+++ main.tf
@@ -7,3 +7,9 @@ resource "azurerm_resource_group" "resource_group" {
     ]
   }
 }
+resource "azurerm_log_analytics_workspace" "central_workspace" {
+  name                = "${var.name}-workspace"
+  location            = "${azurerm_resource_group.resource_group.location}"
+  resource_group_name = "${azurerm_resource_group.resource_group.name}"
+  sku                 = "PerGB2018"
+}
