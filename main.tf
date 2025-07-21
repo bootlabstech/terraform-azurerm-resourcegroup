06da700a10b071a6859063a7554b33b2ac34eefa
lalithaa2407 <lalitalallu02@gmail.com>
2025-07-21 10:05:53 +0530

    rg with keyvault

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.location
@@ -7,4 +6,46 @@ resource "azurerm_resource_group" "resource_group" {
       tags,
     ]
   }
+}
+data "azurerm_client_config" "current" {}
+
+# Creates a Key vault
+resource "azurerm_key_vault" "key_vault" {
+  name                            = var.keyvault_name
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  sku_name                        = var.sku_name
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+  enable_rbac_authorization       = var.enable_rbac_authorization
+  purge_protection_enabled        = var.purge_protection_enabled
+  public_network_access_enabled   = var.public_network_access_enabled
+  soft_delete_retention_days      = var.soft_delete_retention_days
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "SetRotationPolicy",
+    ]
+    secret_permissions = [
+      "Get", "Set", "List", "Recover", "Delete", "Restore"
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Allow"
+
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,access_policy
+    ]
+  }
+  depends_on = [azurerm_resource_group.resource_group]
 }
